970acefe2588596fcd6a6dd751599e49135fe1c8
adriano.pereira@tecsidel.com <admin@example.com>
2019-01-05 13:23:36 -0200

    Initial project & Refactor. Update actix version

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+extern crate actix;
+extern crate tokio_codec;
+extern crate tokio_io;
+
+use actix::prelude::*;
+use actix::actors::resolver::{Connect, Resolver};
+use tokio_codec::{FramedRead, LinesCodec};
+use tokio_io::AsyncRead;
+
+#[derive(Message)]
+pub struct ReceivedLine {
+    pub line: String,
+}
+
+pub struct ConsoleLogger;
+
+impl Actor for ConsoleLogger {
+    type Context = Context<Self>;
+}
+
+impl Handler<ReceivedLine> for ConsoleLogger {
+    type Result = ();
+
+    fn handle(&mut self, message: ReceivedLine, _ctx: &mut Context<Self>) {
+        println!("{}", message.line);
+    }
+}
+
+struct TcpClientActor {
+    recipient: Recipient<ReceivedLine>,
+}
+
+impl Actor for TcpClientActor {
+     type Context = Context<Self>;
+
+     fn started(&mut self, ctx: &mut Self::Context) {
+        println!("TcpClientActor started!");
+
+        Resolver::from_registry()
+            .send(Connect::host("towel.blinkenlights.nl:23"))
+            .into_actor(self)
+            .map(|res, _act, ctx| match res {
+                Ok(stream) => {
+                    println!("TcpClientActor connected!");
+                    let (r, w) = stream.split();
+                    let line_reader = FramedRead::new(r, LinesCodec::new());
+                    ctx.add_stream(line_reader);
+                }
+                Err(err) => {
+                    println!("TcpClientActor failed to connected: {}", err);
+                    ctx.stop();
+                }
+            })
+            .map_err(|err, _act, ctx| {
+                println!("TcpClientActor failed to connected: {}", err);
+                ctx.stop();
+            })
+            .wait(ctx);
+    }
+}
+
+/*impl StreamHandler<String, std::io::Error> for TcpClientActor {
+    fn handle(&mut self, line: String, _ctx: &mut Self::Context) {
+        println!("{}", line);
+    }
+}*/
+
+impl StreamHandler<String, std::io::Error> for TcpClientActor {
+    fn handle(&mut self, line: String, _ctx: &mut Self::Context) {
+        if let Err(error) = self.recipient.do_send(ReceivedLine { line }) {
+            println!("do_send failed: {}", error);
+        }
+    }
+}
+
+fn main() {
+     let sys = actix::System::new("tcp-client-sys");
+
+    let _logger = Arbiter::start(|_| ConsoleLogger);
+
+    let _tcp_client = Arbiter::start(|_| {
+        TcpClientActor { recipient: _logger.recipient() }
+    });
+
+    sys.run();
+}
